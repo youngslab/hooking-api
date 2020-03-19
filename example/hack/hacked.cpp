3ef0d096350d688e199dc057692a502a03d0b6f6
Jaeyoung Park <jaeyoungs.park@gmail.com>
2020-03-19 15:36:36 +0900

    feat) return result with its collaped time

--- example/hack/hacked.cpp
+++ example/hack/hacked.cpp
@@ -4,14 +4,15 @@
 #include <hook/hook.hpp>
 
 std::string version() {
-	// sstatic auto f = hook::load<typeof(version)*>("version");
+  // sstatic auto f = hook::load<typeof(version)*>("version");
   static auto f = hook_load(version);
-	std::cout << "["<< hook_format(version) << "]";
-	return f() + " hacked!";
+  std::cout << "["<< hook_format(version) << "]";
+  return hook_invoke(f) + " hacked!";
 }
 
 int add(int x, int y) {
   static auto f = hook_load(add);
-  std::cout << "["<< hook_format(add, x, y) << "]";
-  return f(x, y);
+  auto [res, dur] = hook_invoke_d(f,x,y);
+  std::cout << "[" << dur.count() <<"ms][" << hook_format(add,x,y) << "]";
+  return res;
 }
--- hook/include/hook/hook.hpp
+++ hook/include/hook/hook.hpp
@@ -6,12 +6,18 @@
 #include <string>
 #include <sstream>
 #include <cstring>
+// invoke
+#include <chrono>
+#include <tuple>
+#include <functional> // std::invoke
 
 #define _str(a) #a
 #define xstr(a) _str(a)
 
 #define hook_load(x) hook::load<typeof(x)*>(xstr(x))
 #define hook_format(f, ...) hook::format(xstr(f), ## __VA_ARGS__)
+#define hook_invoke(...) std::invoke(__VA_ARGS__)
+#define hook_invoke_d(...) hook::invoke_d(__VA_ARGS__)
 
 namespace hook {
 template<typename Func>
@@ -40,6 +46,17 @@ auto format(std::string const &func, Args&&... args) -> std::string {
   return ss.str();
 }
 
+
+template<typename Func , typename... Args>
+auto invoke_d(Func const &f, Args&&... args) -> 
+  std::pair<decltype(f(args...)), std::chrono::milliseconds> {
+	auto start = std::chrono::system_clock::now();
+  auto res = std::invoke(f, std::forward<Args>(args)...);
+  auto dur = std::chrono::system_clock::now() - start;
+  return {res, std::chrono::duration_cast<std::chrono::milliseconds>(dur)}; 
+}
+
+	
 } // namesapce hook
 
 
