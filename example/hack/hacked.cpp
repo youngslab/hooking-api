d04874d7aacf634ff78b6526473728f9cce4bc56
Jaeyoung Park <jaeyoungs.park@gmail.com>
2020-06-23 15:59:51 +0900

    fix) when invoke a func returing void

--- example/hack/hacked.cpp
+++ example/hack/hacked.cpp
@@ -1,20 +1,18 @@
 
 #include <version.h>
-#include <iostream>
 #include <hook/hook.hpp>
-
-std::string xxx(){}
+#include <iostream>
 
 std::string version() {
   // sstatic auto f = hook::load<typeof(version)*>("version");
   static auto f = hook_load(version);
-  std::cout << "["<< hook_format(version) << "]";
+  std::cout << "[" << hook_format(version) << "]";
   return hook_invoke(f) + " hacked!";
 }
 
 int add(int x, int y) {
   static auto f = hook_load(add);
-  auto [res, dur] = hook_invoke_d(f,x,y);
-  std::cout << "[" << dur.count() <<"ms][" << hook_format(add,x,y) << "]";
+  auto[res, dur] = hook_invoke_d(f, x, y);
+  std::cout << "[" << dur.count() << "ms][" << hook_format(add, x, y) << "]";
   return res;
 }
